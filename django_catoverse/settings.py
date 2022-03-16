2753e649c452aadf29e304679ef16c7aabf79091
Ivana505 <ivana.iles@protonmail.com>
2022-03-16 01:06:45 +0000

    Prepared to deploy to heroku

--- django_catoverse/settings.py
+++ django_catoverse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +76,15 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-ivana505-catoverseblog-2oh70e408ve.ws-eu34
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+   # 'default': {
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+  #  }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://qmjzynekhakmiq:04932ba98c99b072e5da3d13ec4095246e98860e750e45308337108bfe65ca8a@ec2-52-19-164-214.eu-west-1.compute.amazonaws.com:5432/da5peh0umqvl02')
 }
 
 
