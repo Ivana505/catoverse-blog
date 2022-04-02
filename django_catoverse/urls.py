b4d4757384fcf075a4882ff1eb2c9aa51119dc4e
Ivana505 <ivana.iles@protonmail.com>
2022-04-02 22:16:00 +0000

    Add account folder into teplates and create html for login,logout and signing up

--- django_catoverse/urls.py
+++ django_catoverse/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')), 
     path('', include('cats.urls'), name='cats_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
