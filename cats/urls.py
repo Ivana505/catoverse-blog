5b12396d6b356a3f0f2d3a0846c2f8d739fa3483
Ivana505 <ivana.iles@protonmail.com>
2022-07-26 23:38:25 +0000

    Change main and header background color

--- cats/urls.py
+++ cats/urls.py
@@ -5,9 +5,12 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('add_post/', views.add_post, name='add_post'),
-    #path('contact/', views.contact name='contact'),
-    path('delete_post/<slug:slug>', views.DeletePostView.as_view(), name='delete_post'),
-    path('delete_comment/<int:cid>/<int:pid>/', views.delete_comment, name='delete_comment'),
+    path(
+        'delete_post/<slug:slug>',
+        views.DeletePostView.as_view(), name='delete_post'),
+    path(
+        'delete_comment/<int:cid>/<int:pid>/',
+        views.delete_comment, name='delete_comment'),
     path('edit_post/<slug:slug>/', views.edit_post, name='edit_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
